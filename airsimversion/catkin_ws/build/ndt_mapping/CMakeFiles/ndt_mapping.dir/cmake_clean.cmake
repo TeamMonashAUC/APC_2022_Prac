41b08b252d4ee24459d521860cb070e62da626e4
yeapzexin <zyea0001@student.monash.edu>
2022-02-19 18:12:20 +0800

    all stuff

---
+++ airsimversion/catkin_ws/build/ndt_mapping/CMakeFiles/ndt_mapping.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ndt_mapping.dir/src/ndt_mapping_node.cpp.o"
+  "CMakeFiles/ndt_mapping.dir/src/ndt_mapping.cpp.o"
+  "/home/yeap/APC_2022_Prac/airsimversion/catkin_ws/devel/lib/ndt_mapping/ndt_mapping.pdb"
+  "/home/yeap/APC_2022_Prac/airsimversion/catkin_ws/devel/lib/ndt_mapping/ndt_mapping"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ndt_mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ airsimversion/catkin_ws/build/ndt_mapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yeap/APC_2022_Prac/airsimversion/catkin_ws/src/ndt_mapping
+# Build directory: /home/yeap/APC_2022_Prac/airsimversion/catkin_ws/build/ndt_mapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
