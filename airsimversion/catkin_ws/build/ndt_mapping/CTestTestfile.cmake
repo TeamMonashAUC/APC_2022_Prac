94fe28649ab442b0165bd0a9de523c6fb96ffd74
yeapzexin <zyea0001@student.monash.edu>
2022-02-19 18:29:09 +0800

    updates

--- airsimversion/catkin_ws/build/ndt_mapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yeap/APC_2022_Prac/airsimversion/catkin_ws/src/ndt_mapping
-# Build directory: /home/yeap/APC_2022_Prac/airsimversion/catkin_ws/build/ndt_mapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
